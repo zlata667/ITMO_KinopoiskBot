907340f2c39d3bd63f7316aa1fd0059c32993c15
galinalar <60663206+galinalar@users.noreply.github.com>
2020-02-25 12:21:30 +0300

    get genre for subscribe

---
+++ src/main/java/com/mycompany/app/FGenre.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import java.util.List;
+
+public class FGenre { //for Genre
+    public List<Info> getItemsInfo() {
+        return itemsInfo;
+    }
+
+    private List<Info> itemsInfo;
+}
---
+++ src/main/java/com/mycompany/app/Info.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import java.util.Map;
+
+public class Info{//for Genre
+    public Map<String, Object> getCoreData() {
+        return coreData;
+    }
+
+    private Map<String, Object> coreData;
+}
